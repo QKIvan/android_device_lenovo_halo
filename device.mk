560661e64c9edaf443d1da86f5bc2ce74fd3531b
basamaryan <basam.aryan@gmail.com>
2024-03-20 01:16:12 -0400

    halo: Don't explicitly include updatable_apex.mk
    It is inherited by base_system.mk as of A14
    
    Change-Id: I020a05b233f61ea28d03199cbc76051e6e97e1dc

--- device.mk
+++ device.mk
@@ -20,9 +20,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_ven
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
-# Enable updatable APEXs
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
